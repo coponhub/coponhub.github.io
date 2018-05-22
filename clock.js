26085e0eec884ff0536b9797b7b7ff91c2009c4a
Kensuke Aoyagi <coponhtc@gmail.com>
2018-05-22 15:08:26 +0900

    Changes to be committed: 	new file:   clock.css 	new file:   clock.js 	new file:   index.html

---
+++ clock.js
@@ -0,0 +1,27 @@
+function updateTime(){
+    	var now = new Date();
+	dateslot.textContent = now.toLocaleDateString();
+	timeslot.textContent =
+	    now.getHours().toString() +
+	    ":" +
+	    ("00" + now.getMinutes()).slice(-2);
+};
+
+window.onload = function(e){ 
+    dateslot = document.getElementById("date");
+    timeslot = document.getElementById("time");
+    updateTime();
+    setInterval(updateTime, 60000);
+};
+
+
+
+
+
+
+
+
+
+
+
+
